80245392ea9ba047fbc9b37826bc07bf4ad5b56c
ShemmyYo <rabiliamemo@gmail.com>
2023-03-04 12:20:54 +0000

    Heroku error h10 - removing web: node index.js from Procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: node index.js
 web: gunicorn PROJ_NAME.wsgi
--- codestar/settings.py
+++ codestar/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
 import os
 import dj_database_url
+
 if os.path.isfile('env.py'):
     import env
 
